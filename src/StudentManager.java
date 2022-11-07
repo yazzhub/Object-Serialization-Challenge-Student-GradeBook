769f637de7796ec3c8f17104d7b9febb397d8313
Yasmin <yasmin@Yasmins-MacBook-Air-2.local>
2022-11-06 18:07:31 -0600

    Step One

---
+++ src/StudentManager.java
@@ -0,0 +1,17 @@
+//Your manager class should have all 4 CRUD methods implemented
+//Your manager class should implement saveData() and loadData() methods
+// and your user interface should give the user an option to save before quitting
+// and to load when first starting the program. Both methods should serialize and deserialize your class data.
+// crud
+// load/save methods
+public class StudentManager {
+
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "Miss Girl";
+        student1.grade = 'A';
+        student1.StudentId = 01;
+
+
+    }
+}
